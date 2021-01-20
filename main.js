319339261c3fa0137cd8541653e720ca8c68eff3
zerodeleo <zerodeleo@users.noreply.github.com>
2021-01-20 15:58:49 +0100

    test

--- main.js
+++
@@ -1,9 +0,0 @@
-// Size of browser viewport.
-$(window).height();
-$(window).width();
-
-// Size of HTML document (same as pageHeight/pageWidth in screenshot).
-$(document).height();
-$(document).width();
-
-document.
